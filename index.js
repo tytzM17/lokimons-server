fa44a6910d1594f52a083f83b5055b4f9de8865c
t1m <azi42@yahoo.com>
2022-08-23 03:01:16 +0800

    data fixes

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-var WebSocketServer = require('ws').Server,
-  wss = new WebSocketServer({port: 40510})
+const WebSocket = require("ws");
 
-wss.on('connection', function (ws) {
-  ws.on('message', function (message) {
-    console.log('received: %s', message)
-  })
+const wss = new WebSocket.Server({ port: 40510 });
 
-  setInterval(
-    () => ws.send(`${new Date()}`),
-    1000
-  )
-})
+wss.getUniqueID = function () {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
+    return s4() + s4() + '-' + s4();
+};
+
+wss.on("connection", ws => {
+    ws.id = wss.getUniqueID();
+    console.log(`New client connected with id: ${ws.id}`);
+
+    ws.onmessage = ({data}) => {
+        console.log(`Client ${ws.id}: ${data}`);
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(data);
+            }
+        });
+    };
+
+    ws.onclose = function() {
+        console.log(`Client ${ws.id} has disconnected!`);
+    };
+});
